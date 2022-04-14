027008feaa078f51472a15c5e4354725b3f5cd60
fchb1239 <fchb1239.dk@gmail.com>
2022-04-14 21:29:53 +0200

    Update PlayerPatch.cs

--- Patches/PlayerPatch.cs
+++ Patches/PlayerPatch.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using HarmonyLib;
 using UnityEngine;
 using SlipperyIce.Behaviours;
@@ -12,23 +12,28 @@ namespace SlipperyIce.Patches
     {
         private static void Postfix()
         {
-            //this guy really changed Level/Forest to Level/forest lmao
-            if (IceHandler.instance == null)
-                GameObject.Find("Level/forest").AddComponent<IceHandler>();
-            else
-                Console.WriteLine("What the hell, how did this happen?");
-            GameObject.Find("Level/forest").AddComponent<Ice>();
+            //just incase lemming pulls a Level/Forest to Level/forest - and he did
+            try
+            {
+                //this guy really changed Level/Forest to Level/forest/Uncover ForestCombined/CombinedMesh-GameObject (1)-mesh/GameObject (1)-mesh-mesh bruh
+                if (IceHandler.instance == null)
+                    GameObject.Find("Level/forest/Uncover ForestCombined/CombinedMesh-GameObject (1)-mesh/GameObject (1)-mesh-mesh").AddComponent<IceHandler>();
+                else
+                    Console.WriteLine("What the hell, how did this happen?");
+                GameObject.Find("Level/forest/pitgeo/pit ground").AddComponent<Ice>();
 
-            /*
-            GameObject.Find("Level/treeroom/tree/Tree").AddComponent<AxisInverter>();
-            foreach (Transform child in GameObject.Find("Level/Forest/SmallTrees").transform)
-                GameObject.Find(string.Format("Level/Forest/SmallTrees/{0}/SmallTree", child.gameObject.name)).AddComponent<AxisInverter>();
-            */
+                /*
+                GameObject.Find("Level/treeroom/tree/Tree").AddComponent<AxisInverter>();
+                foreach (Transform child in GameObject.Find("Level/Forest/SmallTrees").transform)
+                    GameObject.Find(string.Format("Level/Forest/SmallTrees/{0}/SmallTree", child.gameObject.name)).AddComponent<AxisInverter>();
+                */
 
-            if (Plugin.modEnabledTemp)
-                IceHandler.instance.Enable();
+                if (Plugin.modEnabledTemp)
+                    IceHandler.instance.Enable();
 
-            Plugin.modLoaded = true;
+                Plugin.modLoaded = true;
+            }
+            catch { Console.Write("What the aaaaaaa"); }
         }
     }
 }
